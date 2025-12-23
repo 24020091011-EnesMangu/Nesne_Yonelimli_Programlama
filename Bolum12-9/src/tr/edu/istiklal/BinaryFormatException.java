e6da272dbfb8c4b846660d5dd3f1d70e46f7682f
24020091011-EnesMangu <24020091011@ogr.istiklal.edu.tr>
2025-12-23 23:36:06 +0300

    Delete Bolum12-9 directory

--- Bolum12-9/src/tr/edu/istiklal/BinaryFormatException.java
+++
@@ -1,7 +0,0 @@
-package tr.edu.istiklal;
-
-public class BinaryFormatException extends Exception {
-    public BinaryFormatException() {
-        super("Hata: Girilen değer ikili (binary) sayı değildir. Sadece 0 ve 1 olmalıdır.");
-    }
-}
