33ce98ee304b069263762205a00c62dd708e0d1a
24020091011-EnesMangu <24020091011@ogr.istiklal.edu.tr>
2025-12-31 00:53:15 +0300

    Delete Hafta12-24020091011-Enes-Mangu/Hafta12-24020091011-Enes-Mangu/Bolum14-4 directory

--- Hafta12-24020091011-Enes-Mangu/Hafta12-24020091011-Enes-Mangu/Bolum14-4/src/main/java/com/example/bolum144/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.bolum144;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(Bolum14_4.class, args);
-    }
-}
--- Hafta12-24020091011-Enes-Mangu/Hafta12-24020091011-Enes-Mangu/Bolum14-4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.bolum144 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.bolum144 to javafx.fxml;
-    exports com.example.bolum144;
-}
